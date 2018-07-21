ef14221f495b3e52d85dc85801eb40c5b23a8038
sotowang <sotowang@qq.com>
2018-07-21 21:42:25 +0800

    HTTPClient测试

--- src/main/java/com/soto/App.java
+++ src/main/java/com/soto/App.java
@@ -11,11 +11,13 @@ public class App
     static Logger logger = Logger.getLogger(App.class);
 
     public static void main( String[] args ) throws InterruptedException {
-            logger.trace("跟踪信息");
-            logger.debug("调试信息");
-            logger.info("输出信息");
-            logger.warn("警告信息");
-            logger.error("错误信息");
-            logger.fatal("致命信息");
+//            logger.trace("跟踪信息");
+//            logger.debug("调试信息");
+//            logger.info("输出信息");
+//            logger.warn("警告信息");
+//            logger.error("错误信息");
+//            logger.fatal("致命信息");
     }
+
+
 }
--- src/main/java/com/soto/main/JDMain.java
+++ src/main/java/com/soto/main/JDMain.java
@@ -1,13 +1,22 @@
 package com.soto.main;
 
 
+import com.soto.pojo.Book;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
+
+import java.util.List;
 
 public class JDMain {
     private static Logger logger = Logger.getLogger(JDMain.class);
 
     public static void main(String[] args) {
         //初始化httpclient
+        HttpClient httpClient = new DefaultHttpClient();
+        String url = "https://search.jd.com/Search?keyword=java&enc=utf-8&wq=java&pvid=5fe9d3c5a18a4c4f93fd193c64fa9fd0";
+        //抓数据
+//        List<Book> bookList = URLFecter.URLParser(httpClient, url);
     }
 
 }

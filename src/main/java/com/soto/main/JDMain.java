f64338b1419d0906ce81777362f1e008e53ce45d
sotowang <sotowang@qq.com>
2018-07-21 21:12:15 +0800

    Initial commit

---
+++ src/main/java/com/soto/main/JDMain.java
@@ -0,0 +1,14 @@
+package com.soto.main;
+
+
+import org.apache.log4j.Logger;
+
+public class JDMain {
+    private static Logger logger = Logger.getLogger(JDMain.class);
+
+    public static void main(String[] args) {
+        //初始化httpclient
+    }
+
+}
+
---
+++ src/main/java/com/soto/pojo/Book.java
@@ -0,0 +1,31 @@
+package com.soto.pojo;
+
+public class Book {
+    private String bookId;
+    private String bookName;
+    private String bookPrice;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(String bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+}
